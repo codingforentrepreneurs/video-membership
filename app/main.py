c4e787d61fafd27004a759193d569db231688866
CFE <hello@teamcfe.com>
2021-12-05 16:19:02 -0600

    17 - Jinja Templates to Render HTML

--- app/main.py
+++ app/main.py
@@ -1,13 +1,22 @@
-from fastapi import FastAPI
-
+import pathlib
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from cassandra.cqlengine.management import sync_table
 
 from . import config, db
 from .users.models import User
 
+BASE_DIR = pathlib.Path(__file__).resolve().parent # app/
+TEMPLATE_DIR = BASE_DIR / "templates"
+
 app = FastAPI()
+templates = Jinja2Templates(directory=str(TEMPLATE_DIR))
+
 DB_SESSION = None
 # settings = config.get_settings()
+
+
 
 @app.on_event("startup")
 def on_startup():
@@ -17,9 +26,14 @@ def on_startup():
     DB_SESSION = db.get_session()
     sync_table(User)
 
-@app.get("/")
-def homepage():
-    return {"hello": "world"} # json data -> REST API
+
+@app.get("/", response_class=HTMLResponse)
+def homepage(request: Request):
+    context = {
+        "request": request,
+        "abc": "abc"
+    }
+    return templates.TemplateResponse("home.html", context)
 
 
 @app.get("/users")
