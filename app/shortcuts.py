2ecac394969d2cc46b08ec951e9f7d25c8105cd8
CFE <hello@teamcfe.com>
2021-12-07 17:43:07 -0600

    26 - Using Cookies & httponly within FastAPI

--- app/shortcuts.py
+++ app/shortcuts.py
@@ -6,12 +6,20 @@ settings = config.get_settings()
 templates = Jinja2Templates(directory=str(settings.templates_dir))
 
 
-def render(request, template_name, context={}, status_code:int=200):
+def render(request, template_name, context={}, status_code:int=200, cookies:dict={}):
     ctx = context.copy()
     ctx.update({"request": request})
     t = templates.get_template(template_name)
     html_str = t.render(ctx)
     response = HTMLResponse(html_str, status_code=status_code)
-    # set httponly cookies
+    # print(request.cookies)
+    response.set_cookie(key='darkmode', value=1)
+    if len(cookies.keys()) > 0:
+        
+        # set httponly cookies
+        for k, v in cookies.items():
+            response.set_cookie(key=k, value=v, httponly=True)
+    # delete coookies
+    # for key in request.cookies.keys():
+    #     response.delete_cookie(key)
     return response
-    # return templates.TemplateResponse(template_name, ctx, status_code=status_code)
