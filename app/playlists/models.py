39cdc0734c57b89dcb514fe1966f3e0ea37c2d0f
CFE <hello@teamcfe.com>
2021-12-16 13:14:20 -0600

    52 - Playlist Tests in Notebooks

--- app/playlists/models.py
+++ app/playlists/models.py
@@ -5,6 +5,7 @@ from cassandra.cqlengine import columns
 from cassandra.cqlengine.models import Model
 
 from app import config
+from app.videos.models import Video
 
 settings = config.get_settings()
 
@@ -17,4 +18,25 @@ class Playlist(Model):
     updated = columns.DateTime(default=datetime.utcnow())
     host_ids = columns.List(value_type=columns.Text) # ["abc123"]
     title = columns.Text()
-    
+
+    def add_host_ids(self, host_ids=[], replace_all=False):
+        if not isinstance(host_ids, list):
+            return False
+        if replace_all:
+            self.host_ids = host_ids
+        else:
+            self.host_ids += host_ids
+        self.updated = datetime.utcnow()
+        self.save()
+        return True
+
+    def get_videos(self):
+        videos = []
+        for host_id in self.host_ids:
+            try:
+                video_obj = Video.objects.get(host_id=host_id)
+            except:
+                video_obj = None
+            if video_obj is not None:
+                videos.append(video_obj)
+        return videos
