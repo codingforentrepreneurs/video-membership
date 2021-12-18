6ed4f33b1dd0b0ee5d6439b1bd787309f1376924
CFE <hello@teamcfe.com>
2021-12-18 16:44:05 -0600

    61 - Edit or Remove Video via HTMX

--- app/videos/routers.py
+++ app/videos/routers.py
@@ -1,6 +1,6 @@
 import uuid
 from typing import Optional
-
+from starlette.exceptions import HTTPException
 
 from fastapi import APIRouter, Request, Form, Depends
 from fastapi.responses import HTMLResponse
@@ -134,3 +134,65 @@ def video_edit_post_view(
         "object": obj
     }
     return render(request, "videos/edit.html", context)
+
+
+
+@router.get("/{host_id}/hx-edit", response_class=HTMLResponse)
+@login_required
+def video_hx_edit_view(
+    request: Request, 
+    host_id: str, 
+    is_htmx=Depends(is_htmx)):
+    if not is_htmx:
+        raise HTTPException(status_code=400)
+    obj = None
+    not_found = False
+    try:
+        obj = get_object_or_404(Video, host_id=host_id)
+    except:
+        not_found = True
+    if not_found:
+        return HTMLResponse("Not found, please try again.")
+    context = {
+        "object": obj
+    }
+    return render(request, "videos/htmx/edit.html", context) 
+
+
+
+@router.post("/{host_id}/hx-edit", response_class=HTMLResponse)
+@login_required
+def video_hx_edit_post_view(
+        request: Request,
+        host_id: str, 
+        is_htmx=Depends(is_htmx), 
+        title: str=Form(...), 
+        url: str = Form(...),
+        delete: Optional[bool] = Form(default=False)):
+    if not is_htmx:
+        raise HTTPException(status_code=400)
+    obj = None
+    not_found = False
+    try:
+        obj = get_object_or_404(Video, host_id=host_id)
+    except:
+        not_found = True
+    if not_found:
+        return HTMLResponse("Not found, please try again.")
+    if delete:
+        obj.delete()
+        return HTMLResponse('Item Deleted')
+    raw_data = {
+        "title": title,
+        "url": url,
+        "user_id": request.user.username
+    }
+    data, errors = utils.valid_schema_data_or_error(raw_data, VideoEditSchema)
+    if len(errors) > 0:
+        return render(request, "videos/htmx/edit.html", context, status_code=400)
+    obj.title = data.get('title') or obj.title
+    obj.update_video_url(url, save=True)
+    context = {
+        "object": obj
+    }
+    return render(request, "videos/htmx/list-inline.html", context)
