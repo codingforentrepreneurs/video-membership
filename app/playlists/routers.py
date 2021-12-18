ed918b36dda63c8eb595de0e0d6cfbaced977c6c
CFE <hello@teamcfe.com>
2021-12-18 13:04:47 -0600

    58 - Playlist Video Schema

--- app/playlists/routers.py
+++ app/playlists/routers.py
@@ -18,7 +18,7 @@ from app.videos.schemas import VideoCreateSchema
 
 from app.watch_events.models import WatchEvent
 from .models import Playlist
-from .schemas import PlaylistCreateSchema
+from .schemas import PlaylistCreateSchema, PlaylistVideoAddSchema
 
 
 router = APIRouter(
@@ -100,9 +100,10 @@ def playlist_video_add_post_view(
     raw_data = {
         "title": title,
         "url": url,
-        "user_id": request.user.username
+        "user_id": request.user.username,
+        "playlist_id": db_id
     }
-    data, errors = utils.valid_schema_data_or_error(raw_data, VideoCreateSchema)
+    data, errors = utils.valid_schema_data_or_error(raw_data, PlaylistVideoAddSchema)
     redirect_path = data.get('path') or f"/playlists/{db_id}" 
     
     context = {
--- app/playlists/schemas.py
+++ app/playlists/schemas.py
@@ -1,13 +1,64 @@
 import uuid
 from pydantic import (
     BaseModel,
+    validator,
+    root_validator
 )
+
+from app.videos.extractors import extract_video_id
+from app.videos.models import Video
 
 from .models import Playlist
 
 class PlaylistCreateSchema(BaseModel):
     title: str # user generated
     user_id: uuid.UUID # request.session user_id
+
+
+class PlaylistVideoAddSchema(BaseModel):
+    url: str # user generated
+    title: str # user generated
+    user_id: uuid.UUID # request.session user_id
+    playlist_id: uuid.UUID  # Playlist db_id
+
+    @validator("url")
+    def validate_youtube_url(cls, v, values, **kwargs):
+        url = v
+        video_id = extract_video_id(url)
+        if video_id is None:
+            raise ValueError(f"{url} is not a valid YouTube URL")
+        return url
+
+    @validator("playlist_id")
+    def validate_playlist_id(cls, v, values, **kwargs):
+        q = Playlist.objects.filter(db_id=v)
+        if q.count() == 0:
+            raise ValueError(f"{v} is not a valid Playlist")
+        return v
+
+    @root_validator
+    def validate_data(cls, values):
+        url = values.get("url")
+        title = values.get("title")
+        playlist_id = values.get('playlist_id')
+        if url is None:
+            raise ValueError("A valid url is required.")
+        user_id = values.get("user_id")
+        video_obj = None
+        extra_data = {}
+        if title is not None:
+            extra_data['title'] = title
+        try:
+            video_obj, created = Video.get_or_create(url, user_id=user_id, **extra_data)
+        except:
+            raise ValueError("There's a problem with your request, please try again.")
+        if not isinstance(video_obj, Video):
+            raise ValueError("There's a problem with your account, please try again.")
+        if playlist_id:
+            playlist_obj = Playlist.objects.get(db_id=playlist_id)
+            playlist_obj.add_host_ids(host_ids=[video_obj.host_id])
+            playlist_obj.save()
+        return video_obj.as_data()
 
 
         
